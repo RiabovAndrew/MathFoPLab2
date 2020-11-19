57adcff32342f6a7379956f2e1a2457994829064
RiabovAndrew <43186510+RiabovAndrew@users.noreply.github.com>
2020-11-19 21:55:10 +0300

    75% mark

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ auto main(int argc, char* argv[]) -> int {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	auto A = new int[3];
-	A[0] = 1; A[1] = 3; A[2] = 2;
+	auto A = 1;
+	auto B = 1;
+	auto C = 1;
+
+	A = 1; B = 3; C = 2;
 
 
 	int l, r;
@@ -17,67 +20,27 @@ auto main(int argc, char* argv[]) -> int {
 	std::cout << "Введите значение R:";
 	std::cin >> r;
 
-	auto sum = 0;
-	auto mult = 1;
-	auto max1 = A[0];
-	auto max2 = A[0];
-	bool b1 = 0;
-	bool b2 = 0;
-
-	if(A[0] >= l and A[0] <= r) {
-		sum += A[0];
-		mult *= A[0];
-		if(max1<A[0]) {
-			max1 = A[0];
+	if ((A + B + C) > (A * B * C)) {
+		if (A * ((A >= l or A <= r) ? 1 : 0) > B * ((B >= l or B <= r) ? 1 : 0) and A * ((A >= l or A <= r) ? 1 : 0) > C * ((C >= l or C <= r) ? 1 : 0)) {
+			return A;
 		}
-		b1 = 1;
-	}
-	else {
-		if(max2 < A[0]) {
-			max2 = A[0];
-		}
-		b2 = 1;
-	}
-	if (A[1] >= l and A[1] <= r) {
-		sum += A[1];
-		mult *= A[1];
-		if (max1 < A[1]) {
-			max1 = A[1];
-		}
-		b1 = 1;
-	}
-	else {
-		if (max2 < A[1]) {
-			max2 = A[1];
-		}
-		b2 = 1;
-	}
-	if (A[2] >= l and A[2] <= r) {
-		sum += A[2];
-		mult *= A[2];
-		if (max1 < A[2]) {
-			max1 = A[2];
-		}
-		b1 = 1;
-	}
-	else {
-		if (max2 < A[2]) {
-			max2 = A[2];
-		}
-		b2 = 1;
-	}
-	// После испрвления
-	if(b1) {
-		if(sum > mult) {
-			if (b1) std::cout << max1 << "\n";
-			else std::cout << "Выход за массив\n";
+		else if (B * ((B >= l or B <= r) ? 1 : 0) > C * ((C >= l or C <= r) ? 1 : 0) and B * ((B >= l or B <= r) ? 1 : 0) > A * ((A >= l or A <= r) ? 1 : 0)) {
+			return B;
 		}
 		else {
-			if(b2) std::cout << max2 << "\n";
-			else std::cout << "Выход за массив\n";
+			return C;
 		}
 	}
 	else {
-		 std::cout << "Выход за массив\n";
+		if (A * ((A <= l or A >= r) ? 1 : 0) > B * ((B <= l or B >= r) ? 1 : 0) and A * ((A <= l or A >= r) ? 1 : 0) > C * ((C <= l or C >= r) ? 1 : 0)) {
+			return A;
+		}
+		else if (B * ((B <= l or B >= r) ? 1 : 0) > C * ((C <= l or C >= r) ? 1 : 0) and B * ((B <= l or B >= r) ? 1 : 0) > A * ((A <= l or A >= r) ? 1 : 0)) {
+			return B;
+		}
+		else {
+			return C;
+		}
 	}
+
 }
